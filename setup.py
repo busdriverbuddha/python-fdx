36a6f701ba4b696fc0d1647211b31d389e08c3e6
Guilherme Gama <guilhermegama@gmail.com>
2025-04-10 18:49:42 +0000

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-fdx-busdriverbuddha",
-    version="0.2.0",
+    version="0.2.1",
     author="Guilherme Gama",
     author_email="guilhermegama@gmail.com",
     description="FDX Interface",
